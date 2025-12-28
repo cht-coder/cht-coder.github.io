54ac8c5fb21c487a085808bff39c45e4d3525e76
cht-coder <105980463+cht-coder@users.noreply.github.com>
2025-12-28 12:50:53 +0530

    updated astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
+
+  site: 'https://cht-coder.github.io',
 
   vite: {
     plugins: [tailwindcss()],
