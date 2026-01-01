5e6772fc9bae3016b6d52bb8eade3daa9c7a1c02
cht-coder <105980463+cht-coder@users.noreply.github.com>
2026-01-02 00:26:08 +0530

    updated theme & remove unwanted comments

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+export type Theme = 'light' | 'dark';
+
+export const getPreferedTheme = (): Theme => {
+  const saved = localStorage.getItem('theme');
+  if (saved === 'light' || saved === 'dark') return saved;
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light';
+};
