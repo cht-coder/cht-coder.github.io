b53395d1772061dd9f67bbdad1512c9f0c4e8e30
cht-coder <105980463+cht-coder@users.noreply.github.com>
2026-01-04 20:20:13 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [
+    react(),
+    mdx(),
+    sitemap({ namespaces: { image: false, video: false } }),
+  ],
 
   site: 'https://cht-coder.github.io',
   prefetch: true,
