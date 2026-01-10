48a79675cc3232ebbd8f113dfdb0c69f730ff7d5
cht-coder <105980463+cht-coder@users.noreply.github.com>
2026-01-11 02:48:16 +0530

    added react complier

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
+    react({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
     mdx(),
     sitemap({ namespaces: { image: false, video: false } }),
   ],
